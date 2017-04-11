69b176b15c13134ec2e7057f1dc745714f43705e
Herman Tong <ktong1@nd.edu>
2017-04-11 15:26:32 -0400

    search.c compiles but have not tested

--- search.c
+++ search.c
@@ -16,6 +16,30 @@
  * @return  Whether or not the search was successful.
  */
 int	    search(const char *root, const Settings *settings) {
+    struct dirent *dp;
+    DIR *dir = opendir(root);
+
+    if (dir) {
+        while ((dp = readdir(dir)) != NULL) {
+            // generate new path
+            char newpath[strlen(root) + strlen(dp->d_name) + 2];
+            sprintf(newpath, "%s/%s", root, dp->d_name);
+            // pass this directory/file into filter to determine if
+            // it has to be executed
+            if (!filter(newpath, settings))
+                execute(newpath, settings);
+            
+            // check if it is a directory, if so, recurse
+            if (dp->d_type == DT_DIR)
+                search(newpath, settings);
+        }
+    } else {
+        fprintf(stderr, "Error: %s\n", strerror(errno));
+        closedir(dir);
+        return EXIT_FAILURE;
+    }
+
+    closedir(dir);
     return EXIT_SUCCESS;
 }
 
